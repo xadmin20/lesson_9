69b38230568fca307be4af4fcc2f8a67c47eebb6
Nick <74838258+xadmin20@users.noreply.github.com>
2022-09-15 17:41:02 +0300

    Задача №64

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.Write("Введите число для M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число для N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M={M}, N={N}");
+void result(int M, int N)
+{
+  if (M>N) {
+    for (int i = M; i > N; i--) {
+        Console.Write(i + ", ");
+    }
+    Console.WriteLine($"{(N)}");
+  }
+    else if (M<N) {
+    for (int i = M; i < N; i++) { 
+        Console.Write(i + ", ");
+    }
+        Console.WriteLine($"{(N)}");
+    }
+    else { Console.Write("Они не могут быть равны"); }
+}
+result(M, N);
