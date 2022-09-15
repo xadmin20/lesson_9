9b2f5aa02799cfb2c4f17fd03c1854dc857d1305
Nick <74838258+xadmin20@users.noreply.github.com>
2022-09-15 18:01:29 +0300

    Задача №66

--- Program.cs
+++ Program.cs
@@ -1,23 +1,29 @@
-﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 Console.Write("Введите число для M: ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число для N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"M={M}, N={N}");
-void result(int M, int N)
-{
-  if (M>N) {
-    for (int i = M; i > N; i--) {
-        Console.Write(i + ", ");
+void result(int M, int N) {
+    int sum = 0;
+    if (M>N) {
+        for (int i = M; i > N; i--) {
+            sum = sum + i;
+            Console.Write(i + ", ");
+        }
+        Console.WriteLine($"{(N)} сумма будет равна {sum+N}");
     }
-    Console.WriteLine($"{(N)}");
-  }
     else if (M<N) {
     for (int i = M; i < N; i++) { 
+        sum = sum + i;
         Console.Write(i + ", ");
     }
-        Console.WriteLine($"{(N)}");
+        Console.WriteLine($"{(N)} сумма будет равна {sum+N}");
     }
     else { Console.Write("Они не могут быть равны"); }
 }
+
 result(M, N);
